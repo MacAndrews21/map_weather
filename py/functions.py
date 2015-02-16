279bd257380f94acabcad9ef836c81fb6430ef99
MacAndrews21 <walterer.aw@gmail.com>
2015-02-16 23:24:04 +0100

    read date from textfile

--- py/functions.py
+++ py/functions.py
@@ -7,90 +7,103 @@ import csv
 from gdalconst import *
 import postgres as po
 
-##metaData = os.popen('gdalinfo -stats -nomd -norat -noct -nofl ' + filePath + fileName)
-#def getResolution (filePath, fileName):
-    #dataset = gdal.Open(filePath + fileName, GA_ReadOnly)
-    #width = dataset.RasterXSize
-    #height = dataset.RasterYSize
-    ##print 'Projection:', dataset.GetProjection()
+ 
+''' function: delete all ' ' from the file and write the file in an temporary file
+    the original file will not be overwritten
+    Called in: getStations()
+'''
+def cleanFile(filePath, fileName):
+   
+    ''' open file as csv '''
+    csv = open(filePath + fileName, 'r')
+    csvRead = csv.read()
     
-    #geotrans = dataset.GetGeoTransform()
-    #if not geotrans is None:
-        #pixelSize = geotrans[1]
+    ''' replace alls ' ' with '' to clean up the file '''
+    txt = csvRead.replace(' ', '')
     
-    #return width, height, pixelSize
+    ''' close file '''
+    csv.close()
+    
+    ''' write the file in an clean temporary file 
+        this file will be overwritten by the next file
+        => maybe it should be deleted after finish
+    '''
+    writeFile = open('py/temp.txt','w')
+    writeFile.write(txt)
+    writeFile.close() 
 
+''' function: read the first line of the file to get the row names
+    calls the temporary file
+    Callled in: getStations()
+'''    
+def getFirstLine():
+    
+    with open('py/temp.txt', 'rb') as f:
+        firstLine = f.readline()
+        
+        firstLineList = []
+        firstLine = firstLine.replace(' ', '')
+        
+        for index in firstLine.split(';'):
+            if len(index) > 1:
+                #print index, len(index)
+                firstLineList.append(index)
+    
+    return firstLineList
+    
+    
 def getStations(filePath, fileName):
     
-    #csv = open(filePath + fileName, 'r')
-    #csvRead = csv.read()
-    #txt = csvRead.replace(' ', '')
-    #print txt
-    #csv.close()
+    cleanFile(filePath, fileName)
     
-    #temp = open('py/temp.txt','w')
-    #temp.write(txt)
-    #temp.close()
+    firstLineList = getFirstLine()
 
+    
     with open('py/temp.txt', "rb") as csvfile:
         reader = csv.DictReader(csvfile, delimiter = ';')
-        #print 'READER:', reader
+        
         rowList = []
-        a = 0
+        
         for row in reader:
+            #print row
             temp = {}
-            temp['Stations_id'] = row['Stations_id']
-            temp['Stationshoehe'] = row['Stationshoehe']
-            temp['Geogr.Breite']  = row['Geogr.Breite'] 
-            temp['Geogr.Laenge']  = row['Geogr.Laenge'] 
-            temp['von_datum'] = row['von_datum']
-            temp['bis_datum'] = row['bis_datum']
-            temp['Stationsname']  = row['Stationsname'] 
+            for i in firstLineList:
+                #print i
+                temp[i] = row[i]
             
             rowList.append(temp)
-            #rowList.append(row)
+           
         print rowList
  
-  
-getStations("/home/andie/Dropbox/Anita_Andreas/2015 Wetterkarte/tageswerte_00001_19370101_19860630_hist/", "Stationsmetadaten_klima_stationen_00001_19370101_19860630.txt")
+filePath = 'data/tageswerte_00001_19370101_19860630_hist/'
+fileName = 'Stationsmetadaten_klima_stationen_00001_19370101_19860630.txt'
+#getStations("py/", "temp.txt")
+#getStations(filePath, fileName)
 
 
-#def createFileNameList(folderPath):
+def createFileNameList(folderPath):
     
-    #''' create List of file names in folder '''
-    #fileNameList = []
-    
-    
-    
-    #for fileData in glob.glob(os.path.join(folderPath, '*.txt')):
-        #''' create temporary dictionary '''
-        #namesDictionary = {}
+    ''' create List of file names in folder '''
+    fileNameList = []
+   
+    for fileData in glob.glob(os.path.join(folderPath, '*.txt')):
+        ''' create temporary dictionary '''
+        namesDictionary = {}
         
-        #''' seperate filename '''
-        #startIndex = fileData.rfind('/') + 1
-        #fileName = fileData[startIndex:]
-        ##print fileName
+        ''' seperate filename '''
+        startIndex = fileData.rfind('/') + 1
+        fileName = fileData[startIndex:]
+   
         
-        #width, height, pixelSize = getResolution(folderPath, fileName)
+        ''' put fileName into temporary dictionary '''
+        if 'Stationsmetadaten' in fileName:
+            namesDictionary["name"] = fileName
+            
+            ''' add filename to list '''
+            fileNameList.append(namesDictionary)
+        
+        
+    return fileNameList
 
-        
-        #''' get kb id '''        
-        #kb = fileName[:4]
-        
-        #''' get kb year '''        
-        #end = fileName.rfind('.')
-        #year = fileName[5:end]
-        
-        #''' put fileName, id and year into temporary dictionary '''
-        #namesDictionary["name"] = fileName
-        #namesDictionary["id"] = kb
-        #namesDictionary["year"] = year
-        #namesDictionary["width"] = width
-        #namesDictionary["height"] = height
-        #namesDictionary["pixelSize"] = pixelSize
-
-        #''' add filename to list '''
-        #fileNameList.append(namesDictionary)
-        
-        
-    #return fileNameList
+fileNameList = createFileNameList(filePath)
+print fileNameList
