fca5ba59efb4a499b8a1647143d6b8b0f3dfb56d
MacAndrews21 <walterer.aw@gmail.com>
2015-05-02 14:48:25 +0200

    some stuff on the voronoi.sql

--- queries.sql
+++ queries.sql
@@ -125,20 +125,134 @@ INTO test8
 FROM historical AS t1, historical_data AS t2 
 WHERE t2."mess_datum" >= t1."von_datum" AND t2."mess_datum" <= t1."bis_datum" AND t2."stations_id" = t1."stations_id"
 ORDER BY t2."mess_datum"
+--____________________________________________________________________________________________________
+-- DATA CREATE SCRIPT
+--____________________________________________________________________________________________________
+
+-- Create Table 'historical'       
+CREATE TABLE historical
+(
+  id serial NOT NULL,
+  stations_id integer,
+  stationsname character(100),
+  stationshoehe integer,
+  geogr_breite numeric(10,5),
+  geogr_laenge numeric(10,5),
+  von_datum integer,
+  bis_datum integer,
+  geom_4326 geometry(Point,4326),
+  CONSTRAINT pk_historical_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE historical
+  OWNER TO postgres;      
+ 
+-- Create Table 'historical_data'  
+CREATE TABLE historical_data
+(
+  id serial NOT NULL,
+  stations_id integer,
+  mess_datum integer,
+  qualitaets_niveau numeric(10,5),
+  lufttemperatur numeric(10,5),
+  dampfdruck numeric(10,5),
+  bedeckungsgrad numeric(10,5),
+  luftdruck_stationshoehe numeric(10,5),
+  rel_feuchte numeric(10,5),
+  windgeschwindigkeit numeric(10,5),
+  lufttemperatur_maximum numeric(10,5),
+  lufttemperatur_minimum numeric(10,5),
+  lufttemp_am_erdb_minimum numeric(10,5),
+  windspitze_maximum numeric(10,5),
+  niederschlagshoehe numeric(10,5),
+  niederschlagshoehe_ind numeric(10,5),
+  sonnenscheindauer numeric(10,5),
+  schneehoehe numeric(10,5),
+  CONSTRAINT pk_historical_data_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE historical_data
+  OWNER TO postgres;
+
+--____________________________________________________________________________________________________
+-- VIEW CREATE SCRIPT
+--____________________________________________________________________________________________________
+
+-- Create 'lufttemperatur' table 
+SELECT    t1."stations_id"
+        , t1."stationsname"
+        , t1."geom_4326"
+        , t2."lufttemperatur"
+        , t2."mess_datum" 
+INTO temperature_historical
+FROM      historical AS t1
+        , historical_data AS t2 
+WHERE       t2."mess_datum" >= t1."von_datum" 
+        AND t2."mess_datum" <= t1."bis_datum" 
+        AND t2."stations_id" = t1."stations_id" 
+        AND t2."lufttemperatur" > -999
+ORDER BY t2."mess_datum";
+
+
+-- Add Primary Key Column to existing table
+ALTER TABLE temperature_historical ADD COLUMN pid SERIAL PRIMARY KEY;
+
+-- Set owner to user 'postgres'
+ALTER TABLE temperature_historical OWNER TO postgres; 
+
+-- Create Index
+CREATE UNIQUE INDEX temperature_historical_idx ON temperature_historical (pid);
+
+-- How many unique values exists in column 'mess_datum'?
+SELECT mess_datum, COUNT(mess_datum) 
+FROM temperature_historical
+GROUP BY mess_datum
+ORDER BY mess_datum;
+
+-- How many unique values exists in column 'mess_datum'?
+SELECT stations_id, COUNT(stations_id) 
+FROM temperature_historical
+GROUP BY stations_id
+ORDER BY stations_id;
 
 
 
+--____________________________________________________________________________________________________
+--_________________________________________________
+SELECT    t1."stations_id"
+        , t1."stationsname"
+        , t1."geom_4326"
+        , t2."lufttemperatur"
+        , t2."mess_datum" 
+INTO test11
+FROM      historical AS t1
+        , historical_data AS t2 
+WHERE       t2."mess_datum" >= t1."von_datum" 
+        AND t2."mess_datum" <= t1."bis_datum" 
+        AND t2."stations_id" = t1."stations_id" 
+        AND t2."lufttemperatur" > -999
+        AND t2."mess_datum" = 19820121
+ORDER BY t2."mess_datum";
 
 
+SELECT * FROM voronoi('test11', 'geom_4326') AS (id integer, geom_4326 geometry);
 
 
-
-
-
-
-
-
-
+SELECT 
+        ST_DelaunayTriangles(
+                ST_Union(
+                    ST_GeomFromText(
+                        'POLYGON((175 150, 20 40, 50 60,125 100, 175 150))'),
+                    ST_Buffer(
+                        ST_GeomFromText('POINT(110 170)')
+                        , 20)
+                    )
+         ) 
+         As  dtriag;
 
 
 
